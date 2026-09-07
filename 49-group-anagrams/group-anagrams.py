7ef0317bea4716d25f41081a64f63f4b7382cf99
Ashutosh Nayak <ashutoshnayak0987@gmail.com>
2026-09-07 18:41:49 +0530

    Time: 13 ms (49.32%) | Memory: 23.8 MB (26.58%) - LeetSync

--- 49-group-anagrams/group-anagrams.py
+++ 49-group-anagrams/group-anagrams.py
@@ -1,12 +1,18 @@
 class Solution:
     def groupAnagrams(self, strs: List[str]) -> List[List[str]]:
         grp = {}
-        for i in strs:
-            frq = {}
-            for j in i.lower():
-                frq[j] = frq.get(j,0) + 1
-            t = tuple(sorted(frq.items()))
-            grp[t] = grp.get(t, [])
-            grp[t].append(i.lower())
-        return [value for value in grp.values()]
-        
+
+        for word in strs:
+            freq = [0] * 26
+
+            for ch in word:
+                freq[ord(ch) - ord('a')] += 1
+
+            key = tuple(freq)
+
+            if key not in grp:
+                grp[key] = []
+
+            grp[key].append(word)
+
+        return list(grp.values())
