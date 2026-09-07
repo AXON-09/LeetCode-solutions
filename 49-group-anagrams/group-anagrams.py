edbf8e8f40296cf30ae87d2491dd9092b04e601d
Ashutosh Nayak <ashutoshnayak0987@gmail.com>
2026-09-07 18:29:22 +0530

    Time: 27 ms (8.41%) | Memory: 27.3 MB (9.70%) - LeetSync

--- 49-group-anagrams/group-anagrams.py
+++ 49-group-anagrams/group-anagrams.py
@@ -3,9 +3,10 @@ class Solution:
         grp = {}
         for i in strs:
             frq = {}
-            for j in i:
+            for j in i.lower():
                 frq[j] = frq.get(j,0) + 1
             t = tuple(sorted(frq.items()))
-            grp[t] = grp.get(t,[])+[i]
+            grp[t] = grp.get(t, [])
+            grp[t].append(i.lower())
         return [value for value in grp.values()]
         
