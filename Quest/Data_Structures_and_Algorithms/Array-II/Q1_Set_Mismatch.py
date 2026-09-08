b33bdef84ccc705f72c883589cc28fcf591de3f5
Ashutosh Nayak <ashutoshnayak0987@gmail.com>
2026-09-08 11:38:43 +0530

    Add findErrorNums method to detect number mismatch
    Implement findErrorNums method to identify duplicate and missing numbers.

--- Quest/Data_Structures_and_Algorithms/Array-II/Q1_Set_Mismatch.py
+++ Quest/Data_Structures_and_Algorithms/Array-II/Q1_Set_Mismatch.py
@@ -1,1 +1,12 @@
-
+class Solution:
+    def findErrorNums(self, nums: List[int]) -> List[int]:
+        seen = set()
+        lst = set(range(1,len(nums)+1))
+        s = set(nums)
+        for i in range(len(nums)):
+            if nums[i] in seen:
+                p = list(lst-(s))
+                return [nums[i], p[0]]
+            else:
+                seen.add(nums[i]) 
+        
