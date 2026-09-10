0458b27b6cad956c5c43fa7a51601afe4e0dd8db
Ashutosh Nayak <ashutoshnayak0987@gmail.com>
2026-09-11 00:18:50 +0530

    Time: 55 ms (93.07%) | Memory: 19.3 MB (55.68%) - LeetSync

---
+++ 1580-shuffle-the-array/shuffle-the-array.py
@@ -0,0 +1,7 @@
+class Solution:
+    def shuffle(self, nums: List[int], n: int) -> List[int]:
+        arr = [0]*n*2
+        for i in range(n):
+            arr[i*2] = nums[i]
+            arr[(i*2)+1] = nums[n+i]
+        return arr
