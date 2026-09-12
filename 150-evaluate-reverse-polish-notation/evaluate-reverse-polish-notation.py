6944b6c30e4b5b905682090f1a7f4f0d55ebe615
Ashutosh Nayak <ashutoshnayak0987@gmail.com>
2026-09-12 17:33:20 +0530

    Time: 0 ms (100.00%) | Memory: 20.6 MB (36.08%) - LeetSync

--- 150-evaluate-reverse-polish-notation/evaluate-reverse-polish-notation.py
+++ 150-evaluate-reverse-polish-notation/evaluate-reverse-polish-notation.py
@@ -18,7 +18,7 @@ class Solution:
                    r = stack.pop()
                    l = stack.pop()
                    stack.append(l - r)
-        return stack[-1]
+        return stack.pop()
                 
 
         
