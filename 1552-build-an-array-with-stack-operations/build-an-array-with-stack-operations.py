793d760538f47d021796dc8ad5c32b7832f52c2a
Ashutosh Nayak <ashutoshnayak0987@gmail.com>
2026-09-12 17:47:04 +0530

    Time: 2 ms (5.95%) | Memory: 19.2 MB (66.07%) - LeetSync

--- 1552-build-an-array-with-stack-operations/build-an-array-with-stack-operations.py
+++ 1552-build-an-array-with-stack-operations/build-an-array-with-stack-operations.py
@@ -1,15 +1,15 @@
 class Solution:
     def buildArray(self, target: List[int], n: int) -> List[str]:
         target_set = set(target)
-        stack = []
+        Stack = []
 
         for i in range(1, n + 1):
-            stack.append("Push")
+            Stack.append("Push")
 
             if i not in target_set:
-                stack.append("Pop")
+                Stack.append("Pop")
 
             if i == target[-1]:
                 break
 
-        return stack
+        return Stack
