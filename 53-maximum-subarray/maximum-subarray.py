97c0a5d3cf58391d98b99a29c9186bae0fdfe3be
Ashutosh Nayak <ashutoshnayak0987@gmail.com>
2026-09-13 10:49:02 +0530

    Time: 23 ms (91.67%) | Memory: 31.7 MB (6.69%) - LeetSync

--- 53-maximum-subarray/maximum-subarray.py
+++ 53-maximum-subarray/maximum-subarray.py
@@ -4,10 +4,9 @@ class Solution:
         s = 0
         for i in range(len(nums)):
             s += nums[i]
-            m = max(m,s)
+            if s>m:
+                m=s
             if s <= 0:
                s = 0
-
-               
         return m
             
